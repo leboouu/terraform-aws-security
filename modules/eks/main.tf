595542a52533304eac6866f88b34e73fbf5a2c1c
Luffy Taro <luffy@example.com>
2025-11-27 22:17:00 +0000

    Main

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -281,7 +281,7 @@ resource "aws_eks_addon" "ebs_csi_driver" {
   addon_name                  = "aws-ebs-csi-driver"
   addon_version               = var.ebs_csi_driver_addon_version
   service_account_role_arn    = aws_iam_role.ebs_csi_driver.arn
-  resolve_conflicts_on_create = "PRESERVE"
+  resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "PRESERVE"
 
   tags = var.common_tags

2189c5e2534ed7a42ee56b88defc00b79e60d329
Luffy Taro <luffy@example.com>
2025-11-27 20:54:21 +0000

    Main

--- main.tf
+++ main.tf
@@ -150,11 +150,9 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 # ============================================================================

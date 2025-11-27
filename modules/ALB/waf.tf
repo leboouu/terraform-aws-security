83f715901a616dc4461bbd525bdf04d9c5c0b805
Luffy Taro <luffy@example.com>
2025-11-27 14:02:49 +0000

    Main

--- modules/ALB/waf.tf
+++ modules/ALB/waf.tf
@@ -25,16 +25,6 @@ variable "alb_arn" {
   type        = string
 }
 
-variable "project_name" {
-  description = "Project name"
-  type        = string
-}
-
-variable "environment" {
-  description = "Environment name"
-  type        = string
-}
-
 variable "common_tags" {
   description = "Common tags to apply to resources"
   type        = map(string)

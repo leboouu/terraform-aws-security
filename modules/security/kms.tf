b1435de6aa1ae609206b89e19eea2ebc2b86060e
Luffy Taro <luffy@example.com>
2025-11-27 21:45:01 +0000

    Main

--- modules/security/kms.tf
+++ modules/security/kms.tf
@@ -327,6 +327,27 @@ resource "aws_kms_key" "cloudtrail" {
         }
         Action   = "kms:DescribeKey"
         Resource = "*"
+      },
+      {
+        Sid    = "Allow CloudWatch Logs to use the key"
+        Effect = "Allow"
+        Principal = {
+          Service = "logs.${data.aws_region.current.name}.amazonaws.com"
+        }
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:CreateGrant",
+          "kms:DescribeKey"
+        ]
+        Resource = "*"
+        Condition = {
+          ArnLike = {
+            "kms:EncryptionContext:aws:logs:arn" = "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"
+          }
+        }
       }
     ]
   })
